314013130418a848de12c0470fc92e398851b233
samuelw <samuel.wanjohi@nanyukiappfactory.onmicrosoft.com>
2018-10-01 09:48:26 +0300

    create github pages for react-weather-app

---
+++ src/components/Titles.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const Titles = () => {
+  return (
+    <div>
+      <h1 className="title-container__title">Weather Finder</h1>
+      <p className="title-container__subtitle">
+        Find Out Temperature conditions and more
+      </p>
+    </div>
+  );
+};
+
+export default Titles;
